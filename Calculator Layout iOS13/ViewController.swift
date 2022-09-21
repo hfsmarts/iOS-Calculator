455d1cf9642e37fec0ea728f9345329f12e950ff
hfsamrts <harunfazlic@yahoo.com>
2022-09-21 06:47:15 +0200

    adjusting operation button

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     
     @IBOutlet var resultLabel: UILabel!
     
-    
     @IBAction func numOneButton(_ sender: UIButton) {
         if resultLabel.text != "0" && ViewController.opStatus == false{
             resultLabel.text = resultLabel.text! + (sender.titleLabel?.text)!
@@ -37,54 +36,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func plusButton(_ sender: UIButton) {
-        
-        if ViewController.equalDeduplicator == true{
-            if let numValue = resultLabel.text{
-                ViewController.firstValue = Double(numValue)!
-                ViewController.operationHandler = (sender.titleLabel?.text)!
-                
-                print(ViewController.firstValue)
-                print(Double(numValue)!)
-                
-                print(type(of: ViewController.firstValue))
-                print(type(of: (Double(numValue)!)))
-
-                
-            //    if ViewController.firstValue != Double(numValue)!{
-                    let expression = NSExpression(format: "\(ViewController.firstValue)\(ViewController.operationHandler)\(ViewController.finalValue)")
-                    print(expression)
-                    ViewController.finalValue  = (expression.expressionValue(with: nil, context: nil) as? Double)!
-                    if floor(ViewController.finalValue) == ViewController.finalValue{
-                        resultLabel.text = String(format: "%.0f", ViewController.finalValue)
-                    } else {
-                         resultLabel.text = String(format: "%.9f", ViewController.finalValue)
-                        }
-                    
-              //  }
-                
-                
-       
-                
-                
-                
-                
-                ViewController.opStatus = true
-
+                if let numValue = resultLabel.text{
+                    ViewController.firstValue = Double(numValue)!
+                    ViewController.operationHandler = (sender.titleLabel?.text)!
+                    ViewController.opStatus = true
+                    ViewController.equalDeduplicator = false
             }
-            ViewController.equalDeduplicator = false
-
-        }
-        
-        
-        
-        
-        
     }
     
     @IBAction func equalButton(_ sender: UIButton) {
         if ViewController.equalDeduplicator == true{
             if let numValue = resultLabel.text{
                 let expression = NSExpression(format: "\(ViewController.firstValue)\(ViewController.operationHandler)\(numValue)")
+                print(expression)
                 ViewController.finalValue  = (expression.expressionValue(with: nil, context: nil) as? Double)!
                 if floor(ViewController.finalValue) == ViewController.finalValue{
                     resultLabel.text = String(format: "%.0f", ViewController.finalValue)
@@ -105,25 +69,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func plusMinusButton(_ sender: UIButton) {
-        if let numValue = resultLabel.text{
-            if let toDouble = Double(numValue){
-                if toDouble > 0{
-                    resultLabel.text? = "-\(resultLabel.text!)"
-                } else {
-                    resultLabel.text? = String(resultLabel.text!.dropFirst())
+        if Int(resultLabel.text ?? "0") != 0{
+            if let numValue = resultLabel.text{
+                if let toDouble = Double(numValue){
+                    if toDouble > 0{
+                        resultLabel.text? = "-\(resultLabel.text!)"
+                    } else {
+                        resultLabel.text? = String(resultLabel.text!.dropFirst())
+                    }
                 }
             }
         }
     }
     
     @IBAction func percentageButton(_ sender: UIButton) {
-        if let numValue = resultLabel.text{
-            if let toDouble = Double(numValue){
-                ViewController.finalValue = toDouble / 100
+        if Int(resultLabel.text ?? "0") != 0{
+            if let numValue = resultLabel.text{
+                if let toDouble = Double(numValue){
+                        ViewController.finalValue = toDouble / 100
+                }
+                resultLabel.text? = String(ViewController.finalValue)
             }
-            resultLabel.text? = String(ViewController.finalValue)
         }
     }
+    
+    
+    
 }
 
 
