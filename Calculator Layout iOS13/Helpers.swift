a2f1b4abcb482be8084ba071a645553354388e25
hfsamrts <harunfazlic@yahoo.com>
2022-09-28 06:26:36 +0200

    implementing work with decimal numbers and operations

--- Calculator Layout iOS13/Helpers.swift
+++ Calculator Layout iOS13/Helpers.swift
@@ -10,57 +10,25 @@ import UIKit
 import Foundation
 
 
-
-   
 extension ViewController {
   
     func contain(x: Double, y: Double){
         let expression = NSExpression(format: "\(x)\(ViewController.operationHandler)\(y)")
-        let r = ViewController.firstValue.truncatingRemainder(dividingBy: ViewController.firstValue)
-        if r == 0 {
-            ViewController.finalValue  = (expression.expressionValue(with: nil, context: nil) as? Double)!
+   
+        ViewController.finalValue  = (expression.expressionValue(with: nil, context: nil) as? Double)!
+        let isInteger = floor(ViewController.finalValue) == ViewController.finalValue
+        if isInteger {
             resultLabel.text = String(Int(ViewController.finalValue))
         } else {
-            ViewController.finalValue  = (expression.expressionValue(with: nil, context: nil) as? Double)!
             resultLabel.text = String(ViewController.finalValue)
         }
     }
-
-    
-    
-    
-    
-    
 }
 
 
 
 
 
-//extension Decimal {
-//    var significantFractionalDecimalDigits: Int {
-//        return max(-exponent, 0)
-//    }
-//}
-
-
-
-//Code used for decimal extenssion
-
-//resultLabel.text = String(ViewController.finalValue)
-//let n = Decimal(string: resultLabel.text!)
-//var numOfDec = 10
-//if let nUnwrapped = n {
-//    numOfDec = nUnwrapped.significantFractionalDecimalDigits
-//}
-//if numOfDec >= 10{
-//    var value  = UIInterfaceOrientation.landscapeRight.rawValue
-//    if UIApplication.shared.statusBarOrientation == .landscapeLeft || UIApplication.shared.statusBarOrientation == .landscapeRight{
-//        value = UIInterfaceOrientation.portrait.rawValue
-//    }
-//    UIDevice.current.setValue(value, forKey: "orientation")
-//    UIViewController.attemptRotationToDeviceOrientation()
-//}
 
 
 
@@ -70,12 +38,7 @@ extension ViewController {
 
 
 
-//Code that was in plusButton
 
-//if let numValue = resultLabel.text{
-//    ViewController.firstValue = Double(numValue)!
-//    resultLabel.text = numValue
-//    ViewController.operationHandler = (sender.titleLabel?.text)!
-//    ViewController.opStatus = true
-//    ViewController.equalDeduplicator = false
-//}
+
+
+
